32e3c860c6874d57d09100dd2c77e0416cdce060
haji8-de <102823805+haji8-de@users.noreply.github.com>
2023-12-06 13:16:46 +0000

    Pending changes exported from your codespace

--- pitcher/src/app.module.ts
+++ pitcher/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      //debug: false,
+      //playground: false
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema:true
+    }),
+    TodoModule,
+    PrismaModule
+
+  ],
+  providers: [PrismaService],
+  //controllers: [AppController],
+  //providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  
+}
---
+++ pitcher/src/todo/dto/update-todo.input.ts
@@ -0,0 +1,10 @@
+import { CreateTodoInput } from './create-todo.input';
+import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+
+@InputType()
+export class UpdateTodoInput extends PartialType(CreateTodoInput) {
+  @Field(() => Int)
+  id: number;
+  @Field()
+  title: string;
+}
